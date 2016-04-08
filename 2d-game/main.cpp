120271174af96c6edadf6cd8b2c93a7898608b1d
JurMax <jurriaanvdberg@gmail.com>
2016-04-08 23:02:54 +0200

    Initial Commit

---
+++ 2d-game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2d-game
+//
+//  Created by Jurriaan van den Berg on 08-04-16.
+//  Copyright © 2016 JurMax. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
